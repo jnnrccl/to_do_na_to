bd212c0c79f18afd0a5b311e4c47a57c5b786f9c
jnnrccl <80018237+jnnrccl@users.noreply.github.com>
2021-04-19 02:15:06 +0800

    Additional Models Needed for Kanban Screen
    Added BoardItemObject and BoardListObject for Kanban Screen

---
+++ models/board_list_model.dart
@@ -0,0 +1,18 @@
+
+
+import 'board_item_model.dart';
+
+class BoardListObject{
+
+  String title;
+  List<BoardItemObject> items;
+
+  BoardListObject({this.title,this.items}){
+    if(this.title == null){
+      this.title = "";
+    }
+    if(this.items == null){
+      this.items = [];
+    }
+  }
+}
