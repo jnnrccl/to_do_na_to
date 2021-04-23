29581a224cdd8c14c3e42b8b790514356399e7e9
jnnrccl <80018237+jnnrccl@users.noreply.github.com>
2021-04-23 20:56:18 +0800

    Added a new function
    The function gets all the tasks on that selected date in the Calendar.

--- helpers/database_connection.dart
+++ helpers/database_connection.dart
@@ -1,23 +1,27 @@
 import 'dart:io';
+import 'dart:async';
+
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:to_do_na_to/models/task_model.dart';
 
-class DatabaseConnection {
-  static final DatabaseConnection instance = DatabaseConnection._instance();
+import 'package:todo_na_to/models/task_model.dart';
+
+class DatabaseHelper{
+  static final DatabaseHelper instance = DatabaseHelper._instance();
   static Database _db;
 
-  DatabaseConnection ._instance();
+  DatabaseHelper._instance();
 
-  String taskTable = 'task_table';
+  String tasksTable = 'task_table';
   String colId = 'id';
-  String colSubjectName = 'subject_name';
-  String colTaskName = 'task_name';
-  String colPriority = 'priority';
+  String colSubject = 'subjectName';
+  String colTask = 'taskName';
   String colDate = 'date';
+  String colPriority = 'priority';
+  String colStatus = 'status';
 
   Future<Database> get db async {
-    if(_db == null) {
+    if (_db == null){
       _db = await _initDb();
     }
     return _db;
@@ -25,46 +29,56 @@ class DatabaseConnection {
 
   Future<Database> _initDb() async {
     Directory dir = await getApplicationDocumentsDirectory();
-    String path = dir.path + 'task_list.db'; //task_list.db is the name of the database
-    final taskListDb = await openDatabase(path, version: 1, onCreate: _createDb);
-    return taskListDb;
+    String path = dir.path + 'todo_nato.db';
+    final todoListDB = await openDatabase(path, version: 1, onCreate: _createDb);
+    return todoListDB;
   }
 
-  void _createDb(Database db, int version) async {
+  void _createDb(Database db, int version) async{
     await db.execute(
-      "CREATE TABLE $taskTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colSubjectName TEXT, $colTaskName TEXT, $colPriority TEXT, $colDate TEXT)",
-    );
+        'CREATE TABLE $tasksTable($colId INTEGER PRIMARY KEY AUTOINCREMENT, $colSubject TEXT, $colTask TEXT, $colDate TEXT, $colPriority TEXT, $colStatus INTEGER)');
   }
 
   Future<List<Map<String, dynamic>>> getTaskMapList() async {
     Database db = await this.db;
-    final List<Map<String, dynamic>> result = await db.query(taskTable);
+    final List<Map<String, dynamic>> result = await db.query(tasksTable);
     return result;
   }
 
   Future<List<Task>> getTaskList() async {
     final List<Map<String, dynamic>> taskMapList = await getTaskMapList();
     final List<Task> taskList = [];
-    taskMapList.forEach((taskMap) {
+    taskMapList.forEach((taskMap){
       taskList.add(Task.fromMap(taskMap));
     });
     taskList.sort((taskA, taskB) => taskA.date.compareTo(taskB.date));
     return taskList;
   }
 
+  Future<List<Task>> getTasksOnDate(String date) async{
+    Database db = await this.db;
+    final List<Map<String, dynamic>> queryRows = await db.rawQuery('SELECT * FROM task_table WHERE date = ?', [date]);
+    final List<Task> taskList = [];
+    queryRows.forEach((taskMap){
+      taskList.add(Task.fromMap(taskMap));
+    });
+    return taskList;
+
+  }
+
   Future<int> insertTask(Task task) async {
     Database db = await this.db;
-    final int result = await db.insert(taskTable, task.toMap());
+    final int result = await db.insert(tasksTable, task.toMap());
     return result;
   }
 
   Future<int> updateTask(Task task) async {
     Database db = await this.db;
     final int result = await db.update(
-        taskTable,
-        task.toMap(),
-        where: '$colId = ?',
-        whereArgs: [task.id],
+      tasksTable,
+      task.toMap(),
+      where: '$colId = ?',
+      whereArgs: [task.id],
     );
     return result;
   }
@@ -72,10 +86,13 @@ class DatabaseConnection {
   Future<int> deleteTask(int id) async {
     Database db = await this.db;
     final int result = await db.delete(
-      taskTable,
+      tasksTable,
       where: '$colId = ?',
       whereArgs: [id],
     );
     return result;
   }
+
+
+
 }
