9cdc663f501b8ea8be9faa60b2969619e91a615e
jnnrccl <80018237+jnnrccl@users.noreply.github.com>
2021-04-23 20:54:34 +0800

    Changed the format of the date
    Made a function that can convert a DateTime to a certain format in String.
    Function to be applied in mapping the date of the task

--- models/task_model.dart
+++ models/task_model.dart
@@ -1,49 +1,55 @@
-class Task {
+import 'package:intl/intl.dart';
+
+class Task{
+
+  //static String table = "events";
+
   int id;
   String subjectName;
   String taskName;
-  String priority;
   DateTime date;
-  //int status; //0 - task is incomplete, 1 - task is complete
+  String priority;
+  int status; // 0 - incomplete, 1 - complete
 
-  Task({
-    this.subjectName,
-    this.taskName,
-    this.priority,
-    this.date,
-    //this.status,
-  });
-
-  Task.withId({
-    this.id,
-    this.subjectName,
-    this.taskName,
-    this.priority,
-    this.date,
-    //this.status,
-  });
+  Task({this.subjectName, this.taskName, this.date, this.priority, this. status});
+  Task.withID({this.id, this.subjectName, this.taskName, this.date, this.priority, this. status});
 
   Map<String, dynamic> toMap() {
     final map = Map<String, dynamic>();
     if(id != null) {
       map['id'] = id;
     }
-    map['subject_name'] = subjectName;
-    map['task_name'] = taskName;
+    map['id'] = id;
+    map['subjectName'] = subjectName;
+    map['taskName'] = taskName;
+    map['date'] = convertDateTimeDisplay(date.toString()); //date.toIso8601String();
     map['priority'] = priority;
-    map['date'] = date.toIso8601String();
-    //map['status'] = status;
+    map['status'] = status;
     return map;
   }
 
-  factory Task.fromMap(Map<String, dynamic> map) {
-    return Task.withId(
+  factory Task.fromMap(Map<String, dynamic> map){
+    return Task.withID(
       id: map['id'],
-      subjectName: map['subject_name'],
-      taskName: map['task_name'],
+      subjectName: map['subjectName'],
+      taskName: map['taskName'],
+      date: convertStringToDate(map['date']),
       priority: map['priority'],
-      date: DateTime.parse(map['date']),
-      //status: map['status'],
+      status: map['status'],
     );
   }
+}
+
+String convertDateTimeDisplay(String date) {
+  final DateFormat displayFormater = DateFormat('yyyy-MM-dd HH:mm:ss.SSS');
+  final DateFormat serverFormater = DateFormat('yyyy-MM-dd');
+  final DateTime displayDate = displayFormater.parse(date);
+  final String formatted = serverFormater.format(displayDate);
+  return formatted;
+}
+
+DateTime convertStringToDate(String date){
+  String dateWithT = date + 'T0:00:00.00000';
+  DateTime dateTime = DateTime.parse(date);
+  return dateTime;
 }
