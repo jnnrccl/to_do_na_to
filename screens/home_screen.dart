ffed3b1bb087d54026bd0658f408ceaac47e2f65
Daniel Joseph Gultiano <80018717+dcgultiano@users.noreply.github.com>
2021-04-26 17:45:06 +0800

    Modified the Home Screen State
    The Home Screen State is now used for the basic functions of the Home Screen. Task building is not implemented in this state anymore.  A Curved Navigation Bar is implemented to incorporate the "In Progress" and "Completed" categories.

--- screens/home_screen.dart
+++ screens/home_screen.dart
@@ -1,195 +1,47 @@
-import 'dart:io';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';
-import 'package:to_do_na_to/helpers/database_connection.dart';
-import 'package:to_do_na_to/helpers/drawer_navigation.dart';
-import 'package:to_do_na_to/models/task_model.dart';
-import 'package:to_do_na_to/screens/add_task_screen.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:to_do_na_to/screens/calendar_screen.dart';
+import 'package:to_do_na_to/screens/navigations/to_do_list.dart';
+import 'package:to_do_na_to/screens/navigations/completed.dart';
+import 'package:to_do_na_to/screens/navigations/in_progress.dart';
 
-import 'calendar_screen.dart';
-import 'kanban_screen.dart';
+import '../helpers/drawer_navigation.dart';
 
 class HomeScreen extends StatefulWidget {
-  int selectedDestination;
-
-  HomeScreen({this.selectedDestination});
-  
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  Future<List<Task>> _taskList;
-  final DateFormat _dateFormatter = DateFormat('MMM dd, yyyy');
+class _HomeScreenState extends  State<HomeScreen> {
+  int _pageIndex = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
+  final _pageController = PageController();
+  final _navigationPages = [ToDoScreen(), InProgressNavigationScreen(), CompletedNavigationScreen()];
+
+  ScrollController _hideBottomNavController;
+  bool _isVisible;
 
   @override
-  void initState() {
+  initState() {
     super.initState();
-    _updateTaskList();
-  }
-
-  _updateTaskList() {
-    setState(() {
-      _taskList = DatabaseConnection.instance.getTaskList();
-    });
-  }
-
-  Widget _buildTask(snapshot, index, Task task) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 25.0),
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 10.0),
-          Dismissible(
-            key: UniqueKey(),
-            background: Container(
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              margin: EdgeInsets.symmetric(vertical: 3.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.redAccent,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    blurRadius: 2.0,
-                    offset: Offset(0.0, 1.0), // shadow direction: bottom right
-                  )
-                ],
-              ),
-              child: Align(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(left: 15.0),
-                    ),
-                    Text(
-                      "Delete",
-                      style: GoogleFonts.rubik(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            secondaryBackground: Container(
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              margin: EdgeInsets.symmetric(vertical: 3.0),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.redAccent,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    blurRadius: 2.0,
-                    offset: Offset(0.0, 1.0), // shadow direction: bottom right
-                  )
-                ],
-              ),
-              child: Align(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(right: 260.0),
-                    ),
-                    Text(
-                      "Delete",
-                      style: GoogleFonts.rubik(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                  ],
-                ),
-                alignment: Alignment.centerRight,
-              ),
-            ),
-            onDismissed: (direction) {
-              snapshot.data.removeAt(index-1);
-              DatabaseConnection.instance.deleteTask(task.id);
-              _updateTaskList();
-            },
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Container(
-                padding: EdgeInsets.symmetric(vertical: 5.0),
-                margin: EdgeInsets.symmetric(vertical: 3.0),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border(
-                    left: BorderSide(width: 10.5, color: Colors.indigo),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      blurRadius: 2.0,
-                      offset: Offset(0.0, 1.0), // shadow direction: bottom right
-                    ),
-                  ],
-                ),
-                child: ListTile(
-                  leading: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget> [
-                      Container(
-                        width: 95.0,
-                        child: Center(
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              '${task.subjectName}',
-                              style: GoogleFonts.rubik(
-                                color: Colors.indigo,
-                                fontSize: 14.0,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  title: Text(
-                    '${task.taskName}',
-                    style: GoogleFonts.rubik(
-                      color: Colors.indigo,
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  subtitle: Text(
-                    '${_dateFormatter.format(task.date)} • ${task.priority}',
-                    style: GoogleFonts.rubik(
-                      color: Colors.indigo,
-                      fontSize: 12.0,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  onTap: () => Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => AddTaskScreen(
-                      updateTaskList: _updateTaskList,
-                      task: task,
-                    ),),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+    _isVisible = true;
+    _hideBottomNavController = ScrollController();
+    _hideBottomNavController.addListener(() {
+      if (_hideBottomNavController.position.userScrollDirection == ScrollDirection.reverse) {
+        if (_isVisible)
+          setState(() {
+            _isVisible = false;
+          });
+      }
+      if (_hideBottomNavController.position.userScrollDirection == ScrollDirection.forward) {
+        if (!_isVisible)
+          setState(() {
+            _isVisible = true;
+          });
+      }},
     );
   }
 
@@ -197,142 +49,113 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: DrawerNavigation(selectedDestination: 0),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.indigo,
-        child: Icon(
-            Icons.add,
-        ),
-        onPressed: () => Navigator.push(context, MaterialPageRoute(
-          builder: (_) => AddTaskScreen(
-            updateTaskList: _updateTaskList,
-          ),
-          ),
-        ),
-      ),
-      body: FutureBuilder(
-          future: _taskList,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return CustomScrollView(
-              slivers: <Widget>[
-                SliverAppBar(
-                  floating: true,
-                  actions: <Widget>[
-                    IconButton(
-                      icon: Icon(Icons.apps),
-                      onPressed: () => showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return SimpleDialog(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              title: Text(
-                                'Select display view',
-                                style: GoogleFonts.rubik(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                              children: <Widget>[
-                                SimpleDialogOption(
-                                  onPressed: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (_) => HomeScreen(selectedDestination: 0),
-                                    ),
-                                  ),
-                                  child: Text(
-                                    'List',
-                                    style: GoogleFonts.rubik(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ),
-                                SimpleDialogOption(
-                                  onPressed: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (_) => CalendarScreen(selectedDestination: 0),
-                                    ),
-                                  ),
-                                  child: Text(
-                                    'Calendar',
-                                    style: GoogleFonts.rubik(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ),
-                                SimpleDialogOption(
-                                  onPressed: () => Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (_) => KanbanScreen(selectedDestination: 0),
-                                    ),
-                                  ),
-                                  child: Text(
-                                    'Kanban',
-                                    style: GoogleFonts.rubik(
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            );
-                          }
-                      ),
+      body: NestedScrollView(
+        controller: _hideBottomNavController,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              pinned: true,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(MdiIcons.calendar),
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => CalendarScreen(selectedDestination: 0),
                     ),
-                  ],
-                ),
-                SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                      if (index == 0) {
-                        return Container(
-                          padding: EdgeInsets.only(left: 40.0, top: 5.0, right: 40.0, bottom: 26.0),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.only(
-                              bottomRight: Radius.circular(80.0),
-                            ),
-                            color: Colors.indigo,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'Hello there!',
-                                style: GoogleFonts.rubik(
-                                  color: Colors.white,
-                                  fontSize: 35,
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              ),
-                              Text(
-                                'This is your to-do list.',
-                                style: GoogleFonts.rubik(
-                                  color: Colors.white,
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w200,
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      }
-                      return _buildTask(snapshot, index, snapshot.data[index - 1]);
-                    },
-                    childCount: 1 + snapshot.data.length,
                   ),
                 ),
               ],
-            );
-          },
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 35.0, top: 5.0, right: 40.0, bottom: 30.0),
+                    decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(60.0),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.indigo,
+                          blurRadius: 0.0,
+                          spreadRadius: 0.0,
+                          offset: Offset(0, 1),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          _pageIndex == 0 ? 'Hello there!' : 'Tasks',
+                          style: GoogleFonts.rubik(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w200,
+                          ),
+                        ),
+                        Text(
+                          _pageIndex == 0 ? 'This is your to-do list.'
+                          : _pageIndex == 1 ? 'in progress' : 'completed',
+                          style: GoogleFonts.rubik(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ];
+        },
+        body: PageView(
+          children: _navigationPages,
+          onPageChanged: (index) {
+            setState(() {
+              _pageIndex = index;
+            });
+           },
+          controller: _pageController,
         ),
+      ),
+      bottomNavigationBar: AnimatedContainer(
+        duration: Duration(milliseconds: 500),
+        height: _isVisible ? 50.0 : 0.0,
+        child: Wrap(
+          children: <Widget>[
+            CurvedNavigationBar(
+              key: _bottomNavigationKey,
+              index: _pageIndex,
+              height: 50.0,
+              items: <Widget>[
+                Icon(Icons.library_books, size: 20, color: Colors.white),
+                Icon(Icons.library_add, size: 20, color: Colors.white),
+                Icon(Icons.library_add_check, size: 20, color: Colors.white),
+              ],
+              color: Colors.indigo,
+              buttonBackgroundColor: Colors.indigo,
+              backgroundColor: Colors.transparent,
+              animationCurve: Curves.easeInOut,
+              animationDuration: Duration(milliseconds: 500),
+              onTap: (index) {
+                setState(() {
+                  //_showPage = _navigationPage(index);
+                  _pageIndex = index;
+                  _pageController.animateToPage(index, duration: Duration(milliseconds: 500), curve: Curves.linear);
+                });
+              },
+              letIndexChange: (index) => true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
