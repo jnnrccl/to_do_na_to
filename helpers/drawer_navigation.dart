f3f74fa290d449cf1b6cab69c83e9c457c5386eb
Daniel Joseph Gultiano <80018717+dcgultiano@users.noreply.github.com>
2021-04-26 17:56:02 +0800

    Updated code

--- helpers/drawer_navigation.dart
+++ helpers/drawer_navigation.dart
@@ -1,11 +1,12 @@
 import'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:to_do_na_to/screens/analytics_screen.dart';
-import 'package:to_do_na_to/screens/home_screen.dart';
 import 'package:to_do_na_to/screens/scheduler_screen.dart';
 import 'package:to_do_na_to/screens/settings_screen.dart';
 import 'package:to_do_na_to/screens/timer_screen.dart';
+import '../screens/home_screen.dart';
 
+// ignore: must_be_immutable
 class DrawerNavigation extends StatefulWidget {
   int selectedDestination;
   DrawerNavigation({this.selectedDestination});
@@ -15,12 +16,13 @@ class DrawerNavigation extends StatefulWidget {
 
 class _DrawerNavigationState extends State<DrawerNavigation> {
   int _selectedDestination;
-  //var h = AppBar().preferredSize.height;
+
   @override
   void initState(){
     super.initState();
     _selectedDestination = widget.selectedDestination;
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,18 +30,18 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget> [
-             DrawerHeader(
-                  child  : Text('MENU', style: GoogleFonts.rubik(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w200,
-                  )),
-                  decoration: BoxDecoration(
-                      color: Colors.indigo,
-                  ),
-                  margin: EdgeInsets.zero,
-                  padding: EdgeInsets.only(left: 20.0, top: 15.0),
-             ),
+            DrawerHeader(
+              child  : Text('MENU', style: GoogleFonts.rubik(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.w200,
+              )),
+              decoration: BoxDecoration(
+                color: Colors.indigo,
+              ),
+              margin: EdgeInsets.zero,
+              padding: EdgeInsets.only(left: 20.0, top: 15.0),
+            ),
             ListTile(
               leading: Icon(Icons.chrome_reader_mode_outlined),
               title: Text('Dashboard'),
@@ -47,13 +49,9 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
               onTap: () => Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (_) => HomeScreen(selectedDestination:0,),
+                  builder: (_) => HomeScreen(),
                 ),
               ),
-            ),
-            Divider(
-              height: 1,
-              thickness: 1,
             ),
             ListTile(
               leading: Icon(Icons.calendar_today),
@@ -66,10 +64,6 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
                 ),
               ),
             ),
-            Divider(
-              height: 1,
-              thickness: 1,
-            ),
             ListTile(
               leading: Icon(Icons.alarm),
               title: Text('Task Progress Timer'),
@@ -81,10 +75,6 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
                 ),
               ),
             ),
-            Divider(
-              height: 1,
-              thickness: 1,
-            ),
             ListTile(
               leading: Icon(Icons.bar_chart),
               title: Text('Analytics'),
@@ -95,10 +85,6 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
                   builder: (_) => AnalyticsScreen(),
                 ),
               ),
-            ),
-            Divider(
-              height: 1,
-              thickness: 1,
             ),
             ListTile(
               leading: Icon(Icons.notifications_active),
